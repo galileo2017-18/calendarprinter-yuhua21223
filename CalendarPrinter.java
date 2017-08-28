e6fb297da739fffddd24b97e9bd6f2a40b8ad656
Yuhua Wu <yuwu9@s.sfusd.edu>
2017-08-28 19:39:34 +0000

    CanderPrinter.java

--- CalendarPrinter.java
+++ CalendarPrinter.java
@@ -1,7 +1,27 @@
 public class CalendarPrinter {
     public static void main(String[] args) {
         //Type your code here
+        int day = 1;
+        while (day <=30)
         
+        {
+            String message = "September ";
+            if (day == 2 || day == 3 || day == 9 || day == 10||day==16 || day==17||day==23||day==24||day==30 ) {
+                 message = message+day+"- WEEKEND";
+            }
+            else if (day == 4)
+            message = message+day+ " - NO SCHOOL (LABOR DAY)";
+            else  {
+                message = message+day ; 
+                
+            }
+
+            System.out.println(message);
+            day+=1;
+            }
         
-    }
+    }        
+    
 }
+
+
